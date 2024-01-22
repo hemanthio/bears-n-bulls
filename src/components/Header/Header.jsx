44b9e98e3fa842a6ab000b532f2cb238f9545781
hemanthio <121523327+hemanthio@users.noreply.github.com>
2024-01-23 00:23:11 +0530

    navbar

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+
+
+function Header() {
+  return (
+   <>
+   <div className='w-full h-screen bg-green-800
+   text-center pt-16'>
+    <img src={bnbbg} alt="" />
+<div className='font-inter '>
+    <h3 className='text-2xl text-opacity-70 text-white font-[600]'>Welcome to</h3>
+    <h1 className='text-7xl p-2 text-white font-[700]'>Bears N Bulls</h1>
+    <h2 className='text-2xl text-opacity-70 text-white px-3 font-[500]'>Where Strategy Meets Opportunity</h2>
+</div>
+   </div>
+   
+   </>
+  )
+}
+
+export default Header
---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+
+function Navbar() {
+  return (
+   <>
+   <div className='bg-black w-full h-10 flex justify-center items-center'>
+<div>
+    <img className='w-auto h-10 object-cover'
+    src="https://static.vecteezy.com/system/resources/previews/019/766/198/original/apple-logo-apple-icon-transparent-free-png.png" alt="" />
+</div>
+
+<div >
+<ul className='flex gap-2 text-white font-inter font-[400]  text-xs cursor-default'>
+    <li className='hover:bg-slate-100 hover:rounded-2xl px-2 p-1 hover:bg-opacity-20 '>Home</li>
+    <li className='hover:bg-slate-100 hover:rounded-2xl px-2 p-1 hover:bg-opacity-20'>About</li>
+    <li className='hover:bg-slate-100 hover:rounded-2xl px-2 p-1 hover:bg-opacity-20'>News</li>
+    <li className='hover:bg-slate-100 hover:rounded-2xl px-2 p-1 hover:bg-opacity-20'>Events</li>
+    <li className='hover:bg-slate-100 hover:rounded-2xl px-2 p-1 hover:bg-opacity-20'>Members</li>
+</ul>
+</div>
+   </div>
+   
+   </>
+  )
+}
+
+export default Navbar
